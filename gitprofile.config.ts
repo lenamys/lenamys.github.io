0aa472a41dd64c20334b8b9e0a17cbc7009a7cb6
lena ms schumacher <lena.schumacher@fu-berlin.de>
2025-09-28 17:22:30 +0900

    Deployment Test

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -30,7 +30,7 @@ const CONFIG = {
       ],
     },
   },
-  seo: { title: 'Portfolio of Lena Schumacher', description: '', imageURL: '' },
+  seo: { title: 'Portfolio of Lena S.', description: '', imageURL: '' },
   social: {
     linkedin: '',
     x: '',
@@ -120,7 +120,7 @@ const CONFIG = {
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
-  ],*/
+  ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
@@ -132,6 +132,7 @@ const CONFIG = {
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
+  */
   themeConfig: {
     defaultTheme: 'lofi',
     // Hides the switch in the navbar
